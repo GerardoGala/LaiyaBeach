899e2bbc1c2ffd2a74dbd0b48f3c49b751217684
GerardoGala <ggala@hotmail.com>
2026-05-18 16:05:00 +0800

    left pane
    left pane aesthetics

--- js/wind.js
+++ js/wind.js
@@ -39,9 +39,3 @@ export async function initWind() {
   setInterval(fetchWind, 30000);
 }
 
-function degToCompass(num) {
-  const val = Math.floor((num / 22.5) + 0.5);
-  const arr = ["N","NNE","NE","ENE","E","ESE","SE","SSE",
-               "S","SSW","SW","WSW","W","WNW","NW","NNW"];
-  return arr[(val % 16)];
-}
