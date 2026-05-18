ebb44966fec28b230ea7793f5a45eeb0f95746f7
GerardoGala <ggala@hotmail.com>
2026-05-18 09:16:39 +0800

    modified map
    new images

--- js/map.js
+++ js/map.js
@@ -1,6 +1,22 @@
 export function initMap(config) {
-  const laiya = [13.676, 121.437];
-  const buoy = [13.690, 121.450];
+  const laiya = [13.670464, 121.401286];
+  const buoyLat = 13.657641;
+  const buoyLng = 121.407058;
+
+  // Tomato buoy SVG (yellow with orange outline)
+  const buoySVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+      <circle cx="24" cy="24" r="20" fill="yellow" stroke="orange" stroke-width="4"/>
+      <circle cx="24" cy="24" r="8" fill="orange" opacity="0.6"/>
+    </svg>
+  `;
+
+  const buoyIcon = L.icon({
+    iconUrl: "data:image/svg+xml;base64," + btoa(buoySVG),
+    iconSize: [20, 20],
+    iconAnchor: [24, 48],
+    popupAnchor: [0, -48]
+  });
 
   const map = L.map('map').setView(laiya, 14);
 
@@ -8,8 +24,13 @@ export function initMap(config) {
     attribution: '&copy; OpenStreetMap contributors'
   }).addTo(map);
 
+  // Launch point marker
   L.marker(laiya).addTo(map).bindPopup("ILCA Launch Point");
-  L.marker(buoy).addTo(map).bindPopup("Mark Buoy");
+
+  // Buoy marker
+  L.marker([buoyLat, buoyLng], { icon: buoyIcon })
+    .addTo(map)
+    .bindPopup("Race Buoy");
 
   return map;
 }
